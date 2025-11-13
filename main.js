7ec7333773b09da50958a32f0c3e862b4d405086
zakaria allam <zakariaallam536@gmail.com>
2025-11-13 22:09:06 +0100

    drag end drop

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const formulair = document.getElementById('formulair');
 const formAjoute = document.forms['formAjoute']
 
 
+
   formAjoute.addEventListener('submit',function(event){
      event.preventDefault();
     let array = ['image','Fullname','post', 'Nationalite','Prix']
@@ -17,8 +18,8 @@ const formAjoute = document.forms['formAjoute']
   })
 
   if(IsValide){
-      bonTouch.innerHTML += `<div class="w-30  h-47 bg-blue-950 border-blue-500 rounded-b-sm">
-            <img src="1244-1760996162.png" alt="photo de joueur" >
+      bonTouch.innerHTML += `<div draggable="true" class="w-30 relative cursor-pointer  h-47 bg-blue-950 border-blue-500 rounded-b-sm drag ">
+            <img src="/image/${formAjoute.elements['image'].value}.jpg" alt="photo de joueur" >
             <div class="bg-white">${formAjoute.elements['Fullname'].value}</div>
             <div class="bg-amber-400 rounded-[50%] w-8 h-8 content-center font-bold absolute top-0 left-0">${formAjoute.elements['post'].value}</div>
             <div class="bg-red-600">${formAjoute.elements['Nationalite'].value}</div>
@@ -36,3 +37,29 @@ const formAjoute = document.forms['formAjoute']
   afiche_modul.addEventListener("click",()=>{
     formulair.classList.remove('hidden')
   })
+
+  const drag = document.querySelectorAll('.drag');
+  drag.forEach(element =>{
+    element.addEventListener("dragstart",()=>{
+        element.classList.add('isdrag')
+    })
+
+    element.addEventListener("dragend",()=>{
+        element.classList.remove('isdrag')
+    })
+  })
+
+  const drop = document.querySelectorAll('.drop')
+  drop.forEach(element =>{
+    element.addEventListener('dragover',(e)=>{
+        e.preventDefault()
+        const isdrag = document.querySelector('.isdrag')
+        element.appendChild(isdrag)
+    })
+  })
+
+  bonTouch.addEventListener('dragover',(e)=>{
+        e.preventDefault()
+        const isdrag = document.querySelector('.isdrag')
+        bonTouch.appendChild(isdrag)
+    })
